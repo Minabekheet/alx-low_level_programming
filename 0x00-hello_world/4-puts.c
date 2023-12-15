e23f6f23f4430f82c110239a66adb8d4f5c69890
Minabekheet <Mina.heshmat122@gmail.com>
2023-12-15 03:19:06 -0800

    all required tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - programm that prints with put
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
