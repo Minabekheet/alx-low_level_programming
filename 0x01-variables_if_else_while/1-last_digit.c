782cf0721d22fc3b296e6971b7d52f8f200ea318
Minabekheet <Mina.heshmat122@gmail.com>
2023-12-18 08:52:45 -0800

    add

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,6 +23,8 @@ else if (lastnum == 0)
 printf("Last digit of %d is %d and is greater than 0\n", n, lastnum);
 }
 else if (lastnum < 6 && lastnum != 0)
+{
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
+}
 return (0);
 }
