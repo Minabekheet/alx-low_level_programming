a6bfdc9d28929487aa852a59afd74842f88e9bdf
Minabekheet <Mina.heshmat122@gmail.com>
2023-12-21 04:21:21 -0800

    islowertask

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _islower(int c)- to print lower case
+ * int _islower - to print lower case
  * Return: 1 for lower case or 0 for any thing else
  */
 int _islower(int c)
