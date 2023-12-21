b73a8e2eceb9d275bf137bae5e917d685687353c
Minabekheet <Mina.heshmat122@gmail.com>
2023-12-21 04:17:21 -0800

    islowertask

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int _islower(int c)- to print lower case
+ * Return: 1 for lower case or 0 for any thing else
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
