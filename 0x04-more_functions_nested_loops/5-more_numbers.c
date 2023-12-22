169a23bc16f7e9a8111119793a49cd5829babb37
Minabekheet <Mina.heshmat122@gmail.com>
2023-12-22 08:02:13 -0800

    tasks from one to five

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+int i ;
+int x ;
+
+for (i = 0 ; i >= 10 ; i++)
+{
+for (x = 0 ; x >= 14 ; x++)
+{
+if (j >= 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+}
+_putchar('\n');
+}
