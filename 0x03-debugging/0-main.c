d652e3d65fcf202e5db453168e03b023e855816b
Minabekheet <Mina.heshmat122@gmail.com>
2023-12-29 05:45:12 -0800

    firsttask

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests function that print if integer positive or not
+ * Return: 0
+ */
+int main (void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
